43eeb78cbf730c068a433e3f29ab4be1dd4b9c04
Alexey Kravtsov <alexey.kravtsov@dev-pro.net>
2019-02-16 00:03:43 +0200

    TST-9188: test

--- src/app/modules/header/header.module.ts
+++ src/app/modules/header/header.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
 
+// if this line is removed, then all typings generated successfully
 import { CacheService } from 'ng2-cache/ng2-cache';
 
 @NgModule({
